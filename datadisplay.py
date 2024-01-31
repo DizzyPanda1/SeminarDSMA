b6775ce7bc29a42e4fe5b64b073ec12c11609280
DizzyPanda1 <93612839+DizzyPanda1@users.noreply.github.com>
2024-01-31 12:14:14 +0100

    Update datadisplay.py

--- datadisplay.py
+++ datadisplay.py
@@ -4,7 +4,7 @@ import streamlit as st
 import plotly.express as px
 
 try:
-    data = pd.read_csv("cleanedtranslations.csv")
+    data = pd.read_csv("cleanedtranslations.csv", quoting =2)
 except pd.errors.ParserError as e:
     print("Error reading CSV file:", e)
     # Add additional debugging information, if needed
